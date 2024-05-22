32c68c6b3f7c233eeb803d82d34aca3c0d42862a
Mmaduabuchi Elvis Igwilo <148869665+Elhefeelvis1@users.noreply.github.com>
2024-05-22 17:40:34 +0100

    Update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // For Mobile devices
-// Side bar accordion
 const sideBar_acc = document.getElementsByClassName('sideBar-accordion');
+let body = document.querySelector('body');
+// Side bar accordion
+function inputClick(){
+    body.classList.toggle('hidden');
+}
 
 for(i = 0; i < sideBar_acc.length; i++){
     sideBar_acc[i].addEventListener('click', function(){
