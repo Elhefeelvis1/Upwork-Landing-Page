e2839c8b04909dd417aa5bfde380500a77aba5ee
Mmaduabuchi Elvis Igwilo <148869665+Elhefeelvis1@users.noreply.github.com>
2024-05-31 21:45:16 +0100

    Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // For Mobile devices
 const sideBar_acc = document.getElementsByClassName('sideBar-accordion');
-let body = document.querySelector('body');
+const body = document.querySelector('body');
 // Side bar accordion
 function inputClick(){
     body.classList.toggle('hidden');
@@ -12,3 +12,44 @@ for(i = 0; i < sideBar_acc.length; i++){
         this.classList.toggle('active');
     })
 }
+
+// For Bigger screens ****************************
+let subSel = $('header .sub-menu .under1 .sub-sel');
+subSel.hover(function(){
+    subSel.removeClass('active');
+    $(this).addClass('active');
+
+    let talent = $(this).hasClass('tal');
+    let catalog = $(this).hasClass('cat');
+    let consultation = $(this).hasClass('con');
+
+    $('.bigScreen-nav .sub-menu .sub2 .talent').css('display', 'none')
+    $('.bigScreen-nav .sub-menu .sub2 .catalog').css('display', 'none')
+    $('.bigScreen-nav .sub-menu .sub2 .consultations').css('display', 'none')
+
+    switch(true){
+        case talent:
+            $('.bigScreen-nav .sub-menu .sub2 .talent').css('display', 'flex')
+            break;
+        case  catalog:
+            $('.bigScreen-nav .sub-menu .sub2 .catalog').css('display', 'flex')
+            break;
+        case  consultation:
+            $('.bigScreen-nav .sub-menu .sub2 .consultations').css('display', 'flex')
+            break;
+    }
+})
+
+$('header .bigScreen-nav .search-drop .view-box').click(function(){
+    $('header .bigScreen-nav .search-drop .dropdown').toggleClass('active');
+})
+
+$('.bigScreen-nav .search-drop .dropdown .option').click(function(){
+
+    switch (true){
+        case $(this).hasClass('talent'):
+            let talent = $('.bigScreen-nav .search-drop .dropdown .option .talent').text()
+            console.log(talent)
+            $('header .bigScreen-nav .search-drop .view-box p').text(talent)
+    }
+})
